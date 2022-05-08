46a26bd0677190656f9e112535e116eb4b3a1113
Tata-Link <83451272+Tata-git@users.noreply.github.com>
2022-05-08 21:28:04 +0300

    1 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+// console.log(galleryRef);
+let instanceModal;
+
+galleryRef.addEventListener("click", openGallery);
+galleryRef.insertAdjacentHTML("beforeend", galleryElementMarkup(galleryItems));
+
+function openGallery(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName === "IMG") {
+    instanceModal = basicLightbox.create(
+      `
+    <div class="modal">
+        <img src="${event.target.dataset.source}" width="800" height="600" alt="img">
+          </div>
+    `,
+      {
+        onShow: (instanceModal) => {
+          window.addEventListener("keydown", handleClosePopup);
+        },
+
+        onClose: (instanceModal) => {
+          window.addEventListener("keydown", handleClosePopup);
+        },
+      }
+    );
+
+    instanceModal.show();
+    instanceModal.show(() => console.log("lightbox now visible"));
+    // instance.close(() => console.log("lightbox not visible anymore"));
+  }
+}
+function handleClosePopup(e) {
+  if (e.code === "Escape") {
+    console.log("Escape");
+    instanceModal.close();
+  }
+}
+
+function galleryElementMarkup(images) {
+  return images
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+	    </div>`;
+    })
+    .join("");
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+console.log(galleryRef);
+
+galleryRef.insertAdjacentHTML("beforeend", galleryElementMarkup(galleryItems));
+
+new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function galleryElementMarkup(images) {
+  return images
+    .map(({ description, original, preview }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>;
+	    </div>`;
+    })
+    .join("");
+}
